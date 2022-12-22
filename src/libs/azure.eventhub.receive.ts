a1bfa4045d1958c152ba33c692247957896375b0
JayanthiD <jayanthid@wekan.company>
2022-12-22 09:38:05 +0530

    application insights and event hub

---
+++ src/libs/azure.eventhub.receive.ts
@@ -0,0 +1,49 @@
+import { EventHubConsumerClient, earliestEventPosition  }  from "@azure/event-hubs";
+import { ContainerClient } from "@azure/storage-blob";    
+import { BlobCheckpointStore } from "@azure/eventhubs-checkpointstore-blob";
+
+const connectionString = process.env.AZURE_EVENTHUB_CONNECTION_STRING;
+const eventHubName = process.env.AZURE_EVENTHUB_NAME;
+const consumerGroup = "$Default"; // name of the default consumer group
+const storageConnectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const containerName = process.env.AZURE_STORAGE_CONTAINER_NAME;
+
+export default async function receiveEvent() {
+  // Create a blob container client and a blob checkpoint store using the client.
+  const containerClient = new ContainerClient(storageConnectionString, containerName);
+  const checkpointStore = new BlobCheckpointStore(containerClient);
+
+  // Create a consumer client for the event hub by specifying the checkpoint store.
+  const consumerClient = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName, checkpointStore);
+
+  // Subscribe to the events, and specify handlers for processing the events and errors.
+  const subscription = consumerClient.subscribe({
+      processEvents: async (events, context) => {
+        if (events.length === 0) {
+          console.log(`No events received within wait time. Waiting for next interval`);
+          return;
+        }
+
+        for (const event of events) {
+          console.log(`Received event: '${event.body}' from partition: '${context.partitionId}' and consumer group: '${context.consumerGroup}'`);
+        }
+        // Update the checkpoint.
+        await context.updateCheckpoint(events[events.length - 1]);
+      },
+
+      processError: async (err, context) => {
+        console.log(`Error : ${err}`);
+      }
+    },
+    { startPosition: earliestEventPosition }
+  );
+  
+  // After 30 seconds, stop processing.
+  await new Promise((resolve) => {
+    setTimeout(async () => {
+      await subscription.close();
+      await consumerClient.close();
+      resolve(true);
+    }, 30000);
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { UserModule } from './modules/user.module';
 import { ConfigService } from './config/config.service';
 import { ValidationPipe } from '@nestjs/common';
 import { RpcExceptionFilter } from './middlewares/rpc.exception';
+import * as appInsights from "applicationinsights";
+import callLogsSample from './libs/call-logs-sample';
+import  sendEvent  from './libs/azure.eventhub.send';
+// import receiveEvent from './libs/azure.eventhub.receive';
 
 async function bootstrap() {
+  appInsights.setup('InstrumentationKey=811561fb-a13a-489c-9172-93a1f8ea9a2b;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/')
+  .setAutoCollectConsole(true, true)
+  .start();
+  callLogsSample('info', null, null, null, null, "application listening");
   let configService = new ConfigService();
   const app = await NestFactory.createMicroservice(UserModule, {
     transport: Transport.TCP,
@@ -20,6 +28,9 @@ async function bootstrap() {
   }));
 
   app.useGlobalFilters(new RpcExceptionFilter());
+  
+  await sendEvent();
+  // await receiveEvent();
   await app.listen();
 }
 
