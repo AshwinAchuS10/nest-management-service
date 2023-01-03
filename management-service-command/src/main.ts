30f5453392e545cfad76630c624485840be6e90e
Ashwin S <achu10@live.in>
2023-01-03 18:34:19 +0530

    swagger changes

--- management-service-command/src/main.ts
+++ management-service-command/src/main.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from 'configuration/config.service';
 import logger from 'configuration/logger.service';
 import { ExceptionsFilter } from 'middlewares/exception.filter';
@@ -40,7 +40,11 @@ async function bootstrap() {
     app.useGlobalFilters(new ExceptionsFilter());
 
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api', app, document);
+
+    const swaggerOptions: SwaggerCustomOptions = {
+        customSiteTitle: 'Management Services Command API docs'
+    }
+    SwaggerModule.setup('api', app, document, swaggerOptions);
 
     await app.listen(await new ConfigService().get('port'));
 }
